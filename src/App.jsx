01ad22e059b21efedc7da2fd96f73b48246dd906
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-11-13 21:36:20 +0600

    footer component design

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Card from './components/Card/Card';
+import Footer from './components/Footer/Footer';
 import Form from './components/Form/Form';
 import Header from './components/Header/Header';
 import Search from './components/Search/Search';
@@ -10,7 +11,7 @@ function App() {
     <>
       <Header />
       <Form />
-      <div className="p-8">
+      <main className="p-8">
         <div className="max-w-7xl mx-auto space-y-10 px-4">
           {/* <!-- Search, Sort, and Filter Buttons --> */}
           <section className="rounded-3xl border border-neutral-800 bg-gradient-to-br from-neutral-900/80 to-neutral-900/40 p-6 shadow-2xl shadow-black/40 backdrop-blur">
@@ -31,7 +32,8 @@ function App() {
             <Card />
           </div>
         </div>
-      </div>
+      </main>
+      <Footer />
     </>
   );
 }
